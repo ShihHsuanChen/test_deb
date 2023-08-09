455c58fee1a732ca73998f0a6c9b639514da6083
j0018 <j0018@muenai.com>
2023-08-09 12:30:09 +0800

    UPDATE package, README.md

---
+++ README.md
@@ -0,0 +1,24 @@
+# test_deb
+
+test create installer for Linux (debian)
+
+## Install
+
+```sh
+$ pip install -e .
+```
+
+## Run main
+
+```sh
+$ python main.py
+```
+
+
+## Build
+
+```sh
+$ config2cmd build-compose.yml -r build
+```
+
+## Build Installer for debian
--- setup.py
+++ setup.py
@@ -2,18 +2,36 @@ import os
 from setuptools import setup, find_packages
 
 
-def get_requirements(fns):
+def get_requirements(fns, envsub: bool = False):
+    reg = r'\$(\w+)'
     reqs = []
     for fn in fns:
         if not os.path.exists(fn):
             raise FileNotFoundError(f'Given file {fn} does not exists.')
         with open(fn, 'r') as f:
-            reqs += [line.strip() for line in f.readlines()]
+            for line in f.readlines():
+                s = line.strip()
+                if s.startswith('#'):
+                    continue
+                if envsub:
+                    for k in re.findall(reg, line):
+                        v = os.environ.get(k)
+                        if v is None:
+                            warnings.warn(
+                                f'Environment variable "{k}" is required by "{s}"'
+                                f'but not given. Skip'
+                            )
+                            break
+                        s = s.replace('$'+k, v)
+                    else:
+                        reqs.append(s)
+                else:
+                    reqs.append(s)
     return reqs
 
 
 setup(
-    name='myadder',
+    name='mycalc',
     description='Add numbers',
     version_config={
         'template': '{tag}',
